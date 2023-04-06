510e46617c8af75a9a6d5493b47755232e7234d7
mrstijket <mr.stijket@gmail.com>
2023-04-06 15:07:53 +0300

    General Submit

---
+++ Assets/GameDev.tv Assets/Scripts/UI/Inventories/InventoryDragItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using GameDevTV.Core.UI.Dragging;
+
+namespace GameDevTV.UI.Inventories
+{
+    /// <summary>
+    /// To be placed on icons representing the item in a slot. Allows the item
+    /// to be dragged into other slots.
+    /// </summary>
+    public class InventoryDragItem : DragItem<Sprite>
+    {
+    }
+}
